2d6df8115d5cabbdc985535216e581b4f21a6b26
Tepen <elcabron.penyah@gmail.com>
2020-06-28 02:57:57 +0800

    added new Article view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'henlo/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	
+	resources :articles
+
   root 'henlo#index'
 end
