9cbbe25c51dfb63ff4d686edd91a8dab7bfff074
Tepen <elcabron.penyah@gmail.com>
2020-06-28 03:44:27 +0800

    success create new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,15 @@
 class ArticlesController < ApplicationController
 
+	def show
+    @article = Article.find(params[:id])
+  	end
+
 	def new
 	end
 
 	def create
-		@article = Article.new(params[:article])
+		#@article = Article.new(params[:article])
+		@article = Article.new(params.require(:article).permit(:title, :text))
  
 		@article.save
 		redirect_to @article
@@ -12,7 +17,7 @@ class ArticlesController < ApplicationController
 
 	private
   		def article_params
-    		params.require(:article).permit(:title, :text)
+    	params.require(:article).permit(:title, :text)
   		end
 
 end
