764ac447b5cf0b94b187448df17aacd41252a0e9
Tepen <elcabron.penyah@gmail.com>
2020-06-28 13:33:17 +0800

    added basic http authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+	http_basic_authenticate_with name: "tepen", password: "testing123", except: [:index, :show]
+
 	def index
     @articles = Article.all
   	end
